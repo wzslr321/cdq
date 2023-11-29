5965686faf436c9b20062ec3351a680deb605241
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-11-29 21:56:03 +0100

    pretty cringe 10 line mvp but works lmao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let dir_name = args.name;
+    for entry in WalkDir::new("foo") {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if let Some(last_component) = path.file_name() {
+            if last_component.to_str().unwrap() == dir_name {
+                println!("found {}. Path={}", dir_name, path.display());
+            }
+        }
+    }
 }
