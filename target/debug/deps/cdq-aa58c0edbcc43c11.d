e22a9af001499c0ee4e684e2d7572a253c769544
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-11-29 20:28:46 +0100

    initial commit

---
+++ target/debug/deps/cdq-aa58c0edbcc43c11.d
@@ -0,0 +1,5 @@
+/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-aa58c0edbcc43c11.rmeta: src/main.rs
+
+/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-aa58c0edbcc43c11.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cdq-b2e256c1e7089afc.d
@@ -0,0 +1,5 @@
+/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-b2e256c1e7089afc.rmeta: src/main.rs
+
+/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-b2e256c1e7089afc.d: src/main.rs
+
+src/main.rs:
