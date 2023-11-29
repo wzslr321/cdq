d35210f675ddce59ff0ef15260ee3029e5e9ca48
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-11-29 20:29:32 +0100

    add gitignore

--- target/debug/deps/cdq-aa58c0edbcc43c11.d
+++
@@ -1,5 +0,0 @@
-/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-aa58c0edbcc43c11.rmeta: src/main.rs
-
-/Users/wiktor/Remi/rust/cdq/target/debug/deps/cdq-aa58c0edbcc43c11.d: src/main.rs
-
-src/main.rs:
